2250ace2945bc66bba0883aa334f1eb59a4134d9
hossainarif37 <hossainarif4937@gmail.com>
2024-02-16 02:20:01 +0600

    fix: Import issue from user routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
-import { deleteUser, getAllUsers, getUserByRole, getUsersByRole, updatePassword, updateUser } from "../controllers/user.controller";
+import { deleteUser, getAllUsers, getUsersByRole, updatePassword, updateUser } from "../controllers/user.controller";
 import { checkAuth } from "../middleware/authentication";
 
 
